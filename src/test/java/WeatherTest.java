826ce8f4862eaef275080c967438ff43d4676ca2
victor.loboda <victor.loboda@upc.ua>
2019-05-19 19:35:07 +0300

    The second home work - complete getWeatherByCityTest

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -8,7 +8,7 @@ public class WeatherTest {
     @Test
     public void getWeatherPerCityTest(){
         RestAssured.baseURI = "https://pinformer.sinoptik.ua/search.php";
-        ValidatableResponse response = RestAssured.given()
+        ValidatableResponse searchCityResponse = RestAssured.given()
                 .param("lang","ua")
                 .param("return_id",1)
                 .param("q","Lviv")
@@ -17,7 +17,19 @@ public class WeatherTest {
                 .then()
                 //.log().all()
                 .statusCode(200);
-    String cityId = response.extract().asString();
+    String cityId = (searchCityResponse.extract().asString()).substring((searchCityResponse.extract().asString()).lastIndexOf("|")+1);
         System.out.println(cityId);
+
+
+        RestAssured.baseURI = "https://pinformer.sinoptik.ua/pinformer4.php";
+        ValidatableResponse weatherByIdResponse = RestAssured.given()
+                .param("lang","ua")
+                .param("id",cityId)
+                .log().uri()
+                .get()
+                .then()
+                //.log().all()
+                .statusCode(200);
+//https://pinformer.sinoptik.ua/pinformer4.php?type=js&lang=ru&id=303014487
     }
 }
